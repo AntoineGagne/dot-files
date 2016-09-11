067fbb6e47d80e9d990ce865de319c73dec1e987
Antoine Gagné <gagnantoine@gmail.com>
2016-09-11 17:15:43 -0400

    Adds a script to execute all the scripts

---
+++ configure.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+./link_dot_files.sh \
+    && ./nvim_setup.sh \
+    && ./setup_virtual_envs.sh
--- nvim_setup.sh
+++ nvim_setup.sh
@@ -2,10 +2,10 @@
 
 # Clone the `Vundle` plugin manager
 mkdir -p ~/.vim/bundle \
-&& ln -sf "$(pwd)/colors" "~/.vim" \
-&& git clone git@github.com:VundleVim/Vundle.vim.git "~/.vim/bundle"
+    && ln -sf "$(pwd)/colors" "~/.vim" \
+    && git clone git@github.com:VundleVim/Vundle.vim.git "~/.vim/bundle"
 
 # Setup `Neovim`
 mkdir -p "~/.config" \
-&& ln -s "~/.vim ~/.config/nvim" \
-&& ln -s "~/.vimrc ~/.config/nvim/init.vim"
+    && ln -s "~/.vim ~/.config/nvim" \
+    && ln -s "~/.vimrc ~/.config/nvim/init.vim"
