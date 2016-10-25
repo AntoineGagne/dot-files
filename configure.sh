1861320970014b70c495d00cd8c5c95114de1255
Antoine Gagné <gagnantoine@gmail.com>
2016-10-25 17:11:44 -0400

    Adds `redshift`
    * Adds `redshift` configuration to the configuration files
    * Adds a script to link redshift

--- configure.sh
+++ configure.sh
@@ -2,4 +2,5 @@
 
 ./link_dot_files.sh \
     && ./nvim_setup.sh \
-    && ./setup_virtual_envs.sh
+    && ./setup_virtual_envs.sh \
+    && ./link_redshift.sh
---
+++ link_redshift.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+mkdir -p ~/.config && ln -sf "$(pwd)/redshift" ~/.config/
