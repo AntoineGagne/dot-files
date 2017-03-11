8d0f6d3e42a47a55c6a1085ed2ce809f34e3c68d
Antoine Gagné <gagnantoine@gmail.com>
2017-03-11 10:26:59 -0500

    Change directory structure

--- link_dot_files.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-for file in $(find . -name ".[^.]*" ! -path "./.git"); do
-    ln -sf "$(pwd)/${file:2}" "${1}"
-done
--- nvim_setup.sh
+++ nvim_setup.sh
@@ -1,15 +1,5 @@
 #! /bin/sh
 
-# Clone the `Vundle` plugin manager
-mkdir -p "${HOME}/.vim/bundle" \
-    && ln -sf "$(pwd)/colors" "${HOME}/.vim/" \
-    && git clone git@github.com:VundleVim/Vundle.vim.git "${HOME}/.vim/bundle"
-
-# Setup `Neovim`
-mkdir -p "${HOME}/.config" \
-    && ln -sf "${HOME}/.vim" "${HOME}/.config/nvim" \
-    && ln -sf "${HOME}/.vimrc" "${HOME}/.config/nvim/init.vim"
-
 # Install the plugins
 nvim +PluginInstall +qa \
     && cd ~/.vim/bundle/vimproc.vim \
