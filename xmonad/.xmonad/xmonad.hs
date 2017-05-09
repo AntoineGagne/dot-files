fe231eab5f22e6c779151a65023fc0c7278af49d
Antoine Gagné <gagnantoine@gmail.com>
2017-05-08 20:20:46 -0400

    Add auto managed app configuration

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,14 +1,86 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Spiral
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
 import XMonad.Util.Run (spawnPipe, hPutStrLn)
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as Map
 
 
 main = do
-    xmobarStatusBar <- xmobar $ def { terminal = terminalName
-                                   , modMask = mod4Mask
-                                   , borderWidth = 1
-                                   }
-    xmonad xmobarStatusBar
+    barHandle <- spawnPipe "xmobar"
+    xmonad $ defaults 
+        { logHook = dynamicLogWithPP $ xmobarPP
+            { ppOutput = hPutStrLn barHandle
+            , ppTitle = xmobarColor xmobarTitleColor "" . shorten 50
+            , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+            , ppSep = "   "
+            }
+        , layoutHook = avoidStruts $ layoutHook def
+        , manageHook = manageDocks <+> myManageHooks <+> manageHook def
+        -- To make Java applications behave normally...
+        , startupHook = setWMName "LG3D"
+        }
 
-statusBarName = "xmobar"
-terminalName = "urxvtc"
+defaults = def
+    { borderWidth = myBorderWidth
+    , focusFollowsMouse = myFocusFollowsMouse
+    , focusedBorderColor = myFocusedBorderColor
+    , manageHook = myManageHooks
+    , modMask = mod4Mask
+    , mouseBindings = myMouseBindings
+    , normalBorderColor  = myNormalBorderColor
+    , terminal = myTerminal
+    , workspaces = myWorkspaces
+    }
+
+myModMask = mod4Mask
+myBorderWidth = 1
+myStatusBar = "xmobar"
+myTerminal = "urxvtc"
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = True
+
+myNormalBorderColor  = "#7c7c7c"
+myFocusedBorderColor = "#ffb6b0"
+
+-- Colors for text and backgrounds of each tab when in "Tabbed" layout.
+tabConfig = def {
+    activeBorderColor = "#7C7C7C",
+    activeTextColor = "#CEFFAC",
+    activeColor = "#000000",
+    inactiveBorderColor = "#7C7C7C",
+    inactiveTextColor = "#EEEEEE",
+    inactiveColor = "#000000"
+}
+-- Color of current window title in xmobar.
+xmobarTitleColor = "#FFB6B0"
+-- Color of current workspace in xmobar.
+xmobarCurrentWorkspaceColor = "#CEFFAC"
+
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = Map.fromList $
+    [ -- mod-button1, Set the window to floating mode and move by dragging
+      ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    -- mod-button2, Raise the window to the top of the stack
+    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    -- mod-button3, Set the window to floating mode and resize by dragging
+    , ((modMask, button3), (\w -> focus w >> mouseResizeWindow w))
+    -- you may also bind events to the mouse scroll wheel (button4 and button5)
+    ]
+
+myWorkspaces = ["1:web", "2:term", "3:read", "4:code", "5:media"] ++ map show [6..9]
+myManageHooks = composeAll
+    [ className =? "URxvt" --> doShift "2:term"
+    , className =? "Firefox" --> doShift "1:web"
+    , className =? "Zathura" --> doShift "3:read"
+    , className =? "MPlayer" --> doShift "5:media"
+    ]
+
