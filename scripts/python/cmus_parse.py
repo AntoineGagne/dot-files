02b24e070e179cf297d691511272e5e84373ab6f
Antoine Gagné <gagnantoine@gmail.com>
2017-05-11 21:05:34 -0400

    Change maximum song width

--- scripts/python/cmus_parse.py
+++ scripts/python/cmus_parse.py
@@ -192,7 +192,7 @@ if __name__ == '__main__':
                 STATUS_INFORMATION.status,
                 sys.argv[1].format(**STATUS_INFORMATION.__dict__)
             ),
-            width=80,
+            width=60,
             placeholder='…'
         )
     )
