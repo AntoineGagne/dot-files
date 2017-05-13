0bb1da0a177ba6a637f133015a1ca41a98945899
Antoine Gagné <gagnantoine@gmail.com>
2017-05-13 17:11:03 -0400

    Remove systemd recipe

---
+++ scripts/bash/fix-systemd-user-units.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+systemd_units_directory="${HOME}/.config/systemd/user"
+
+if [ ! -f "${systemd_units_directory}/${1}" ]; then
+    # Swap all the `~` for the absolute `${HOME}` path
+    sed -i 's@~@'"${HOME}"'@g@' "${1}"
+    cp "${1}" "${systemd_units_directory}/"
+fi
