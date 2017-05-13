044ed7a67035b336c4c4a56f5a473c2100f422ed
Antoine Gagné <gagnantoine@gmail.com>
2017-05-13 12:06:52 -0400

    Fix script according to shellcheck

--- scripts/bash/control-volume.bash
+++ scripts/bash/control-volume.bash
@@ -7,7 +7,7 @@ toggle_sound() {
 
 control_volume() {
     pactl set-sink-mute @DEFAULT_SINK@ false
-    pactl set-sink-volume @DEFAULT_SINK@ $1
+    pactl set-sink-volume @DEFAULT_SINK@ "$1"
     write_volume_status
 }
 
@@ -16,12 +16,12 @@ create_volume_pipe() {
         rm "$HOME/.volume"
     fi
     mkfifo "$HOME/.volume"
-    echo "$(write_volume_status)" > "$HOME/.volume" &
+    write_volume_status >"$HOME/.volume" &
 }
 
 write_volume_status() {
     if [ "$(is_muted)" = "no" ]; then
-        echo "$(get_volume_level)" > "$HOME/.volume" &
+        get_volume_level >"$HOME/.volume" &
     else
         echo "Muted" > "$HOME/.volume" &
     fi
@@ -29,16 +29,16 @@ write_volume_status() {
 
 is_muted() {
     SINK_NUMBER="$(get_default_sink_number)"
-    echo "$(pactl list sinks | perl -000ne 'if(/#$ENV{'SINK_NUMBER'}/){/(Mute:\s*(.*)\s*)/; print "$2\n"}')"
+    pactl list sinks | perl -000ne 'if(/#$ENV{'SINK_NUMBER'}/){/(Mute:\s*(.*)\s*)/; print "$2\n"}'
 }
 
 get_volume_level() {
     SINK_NUMBER="$(get_default_sink_number)"
-    echo "$(pactl list sinks | perl -000ne 'if(/#$ENV{'SINK_NUMBER'}/){/Volume:\s*front-left:.*\/\s*(\d+%).*front-right:.*\/\s*(\d+%).*/; print "$1 $2\n"}')"
+    pactl list sinks | perl -000ne 'if(/#$ENV{'SINK_NUMBER'}/){/Volume:\s*front-left:.*\/\s*(\d+%).*front-right:.*\/\s*(\d+%).*/; print "$1 $2\n"}'
 }
 
 get_default_sink_number() {
-    echo "$(pacmd list-sinks | perl -000ne 'if (/\*\s*index/){/(\*\s*index:\s*(\d+))/; print "$2\n"}')"
+    pacmd list-sinks | perl -000ne 'if (/\*\s*index/){/(\*\s*index:\s*(\d+))/; print "$2\n"}'
 }
 
 case "$1" in
@@ -46,13 +46,13 @@ case "$1" in
         create_volume_pipe
         ;;
     -v|--volume)
-        echo "$(get_volume_level)"
+        get_volume_level
         ;;
     -d|--default-sink)
-        echo "$(get_default_sink_number)"
+        get_default_sink_number
         ;;
     -m|--muted)
-        echo "$(is_muted)"
+        is_muted
         ;;
     -t|--toggle)
         toggle_sound
