cc014bbfec4d36b7e7c0ed197c8b76ac60df5085
Antoine Gagné <gagnantoine@gmail.com>
2017-05-13 17:13:26 -0400

    Fix script

--- scripts/bash/fix-systemd-user-units.sh
+++ scripts/bash/fix-systemd-user-units.sh
@@ -2,7 +2,7 @@
 
 systemd_units_directory="${HOME}/.config/systemd/user"
 
-if [ ! -f "${systemd_units_directory}/${1}" ]; then
+if [ ! -f "${systemd_units_directory}/$(basename "$1")" ]; then
     # Swap all the `~` for the absolute `${HOME}` path
     sed -i 's@~@'"${HOME}"'@g@' "${1}"
     cp "${1}" "${systemd_units_directory}/"
