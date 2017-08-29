f868eba62c041fc08859b24a60befbc9dbe18ef3
Antoine Gagné <gagnantoine@gmail.com>
2017-08-29 12:26:17 -0400

    Fix bug in script

--- scripts/python/capitalize_all_files.py
+++ scripts/python/capitalize_all_files.py
@@ -4,22 +4,24 @@
 from os import listdir, rename
 from os.path import isfile, join
 from string import capwords
-from typing import List
 
 import sys
 
 
-def capitalize_file_names(files: List[str]):
+def capitalize_file_names(files, path):
     """Capitalizes all the given files.
     :param files: The files to capitalize
     :type files: list<str>
     """
     for filename in files:
-        rename(filename, capwords(filename))
+        rename(
+            join(path, filename),
+            join(path, capwords(filename))
+        )
 
 
 if __name__ == '__main__':
     DIRECTORY_PATH = sys.argv[1]
     FILES = (single_file for single_file in listdir(DIRECTORY_PATH)
              if isfile(join(DIRECTORY_PATH, single_file)))
-    capitalize_file_names(FILES)
+    capitalize_file_names(FILES, DIRECTORY_PATH)
