c368acf1e02abddac914b65c393c03a1e2d3e385
Antoine Gagné <gagnantoine@gmail.com>
2017-08-31 15:19:38 -0400

    Sort the URL after merging

--- scripts/bash/merge-rss-urls.bash
+++ scripts/bash/merge-rss-urls.bash
@@ -4,4 +4,4 @@ newsbeuter_directory="${HOME}/.newsbeuter"
 private_urls_file="${newsbeuter_directory}/private"
 public_urls_file="${newsbeuter_directory}/public"
 
-paste -d "\n" "${private_urls_file}" "${public_urls_file}" | grep -v '^$' > "${newsbeuter_directory}/urls"
+paste -d "\n" "${private_urls_file}" "${public_urls_file}" | grep -v '^$' | sort -u > "${newsbeuter_directory}/urls"
