df5a61c4a792cadd45961000756e8c2519a47fb8
Antoine Gagné <gagnantoine@gmail.com>
2017-10-12 19:57:36 -0400

    Change font from bold to normal

--- firefox/.homepage/js/homepage.js
+++ firefox/.homepage/js/homepage.js
@@ -1,4 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const currentDate = new Date(Date.now()).toLocaleDateString();
-    document.getElementById("current-date-text").textContent = currentDate;
+"use strict";
+
+const whenDocumentIsReady = new Promise(resolve => {
+    document.addEventListener("DOMContentLoaded", resolve);
 });
+
+const displayCurrentDate = function () {
+    const currentDate = new Date(Date.now());
+    const currentDateNode = document.getElementById("current-date-text");
+    currentDateNode.textContent = currentDate.toDateString();
+}
+
+whenDocumentIsReady.then(displayCurrentDate);
