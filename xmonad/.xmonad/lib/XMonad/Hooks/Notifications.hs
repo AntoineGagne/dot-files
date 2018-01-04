bf247a582ff92e995ff1028255c2816dac85e452
Antoine Gagné <gagnantoine@gmail.com>
2018-01-04 15:03:41 -0500

    Use haskell library to send notifications

--- xmonad/.xmonad/lib/XMonad/Hooks/Notifications.hs
+++ xmonad/.xmonad/lib/XMonad/Hooks/Notifications.hs
@@ -2,14 +2,36 @@ module XMonad.Hooks.Notifications
     ( LibNotifyUrgencyHook (..)
     ) where
 
+import Control.Monad ( void )
+import Data.Maybe ( Maybe
+                  , fromMaybe
+                  )
+import DBus.Notify ( blankNote
+                   , connectSession
+                   , notify
+                   , Body (..)
+                   , Client
+                   , Note (..)
+                   , Hint ( Urgency )
+                   , UrgencyLevel ( Low )
+                   )
 import XMonad ( gets
               , windowset
+              , MonadIO (..)
               )
+import XMonad.Core ( catchIO )
+import XMonad.Hooks.DynamicLog ( dzenStrip
+                               , xmobarStrip
+                               )
 import XMonad.Hooks.UrgencyHook ( UrgencyHook (..) )
-import XMonad.Util.NamedWindows ( getName )
+import XMonad.Layout.IndependentScreens ( unmarshallS
+                                        , unmarshallW
+                                        )
+import XMonad.StackSet ( findTag )
+import XMonad.Util.NamedWindows ( getName
+                                , NamedWindow (..)
+                                )
 import XMonad.Util.Run ( safeSpawn )
-
-import qualified XMonad.StackSet as StackSet
 
 data LibNotifyUrgencyHook = LibNotifyUrgencyHook
     deriving (Read, Show)
@@ -17,5 +39,20 @@ data LibNotifyUrgencyHook = LibNotifyUrgencyHook
 instance UrgencyHook LibNotifyUrgencyHook where
     urgencyHook LibNotifyUrgencyHook window = do
         name <- getName window
-        Just workspaceId <- StackSet.findTag window <$> gets windowset
-        safeSpawn "notify-send" [show name, "workspace " ++ workspaceId]
+        workspaceId <- findTag window <$> gets windowset
+        sendNotification $ createNote name workspaceId
+
+createNote :: Show i => NamedWindow -> Maybe i -> Note
+createNote namedWindow workspaceId = blankNote
+    { summary = "Workspace: " ++ fromMaybe "n/a" workspace
+    , appName = "Notification"
+    , body = Just . Text . show $ namedWindow
+    , hints = [Urgency Low]
+    }
+        where
+            workspace = dzenStrip . xmobarStrip . show <$> workspaceId
+
+sendNotification :: MonadIO m => Note -> m ()
+sendNotification note = do
+    client <- liftIO connectSession
+    catchIO . void $ notify client note
