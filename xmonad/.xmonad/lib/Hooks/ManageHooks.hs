4948b1fcc7cf54eca2b75cc28fd2e500f092861e
Antoine Gagné <gagnantoine@gmail.com>
2018-03-04 20:01:27 -0500

    Add JFLAP to the managed windows

--- xmonad/.xmonad/lib/Hooks/ManageHooks.hs
+++ xmonad/.xmonad/lib/Hooks/ManageHooks.hs
@@ -47,6 +47,7 @@ myManageHooks screenNumber = composeAll
     , className =? "jetbrains-idea" <&&> resource =? "Dialog" --> moveToWorkspace [1] 2
     , className =? "jetbrains-pycharm" --> moveToWorkspace [1] 2
     , className =? "jetbrains-pycharm" <&&> resource =? "Dialog" --> moveToWorkspace [1] 2
+    , className =? "JFLAP" --> moveToWorkspace [1] 2
     , className =? "Easytag" --> moveToWorkspace [1] 4
     , className =? "MPlayer" --> moveToWorkspace [1] 4
     , className =? "mpv" --> moveToWorkspace [1] 4
