cda147221b192d8f2b30b3ccf39fe83373ec127f
Antoine Gagné <gagnantoine@gmail.com>
2020-12-13 19:44:30 -0500

    Add more tests

--- xmonad/.xmonad/test/Programs/BrightnessSpec.hs
+++ xmonad/.xmonad/test/Programs/BrightnessSpec.hs
@@ -2,6 +2,7 @@ module Programs.BrightnessSpec where
 
 import Data.Maybe
   ( fromJust,
+    isJust,
   )
 import Programs.Brightness
   ( BrightnessDevice,
@@ -13,36 +14,42 @@ import Programs.Brightness
     percent,
   )
 import Test.Hspec
-import Test.QuickCheck
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-  describe "decreaseBy" $
-    parallel $ do
-      it "decreases by a correct amount" $
-        decreaseBy device' (percent' 10) `shouldBe` dummyDevice 1000 900 1000
+spec = parallel $ do
+  describe "decreaseBy" $ do
+    it "decreases by a correct amount" $
+      decreaseBy device' (percent' 10) `shouldBe` dummyDevice 1000 900 1000
 
-      it "does not decrease less than 0" $
-        decreaseBy (dummyDevice 0 0 1000) (percent' 10) `shouldBe` dummyDevice 0 0 1000
+    it "does not decrease less than 0" $
+      decreaseBy (dummyDevice 0 0 1000) (percent' 10) `shouldBe` dummyDevice 0 0 1000
 
-  describe "increaseBy" $
-    parallel $ do
-      it "increases by a correct amount" $
-        increaseBy (dummyDevice 1000 900 1000) (percent' 10) `shouldBe` dummyDevice 1000 1000 1000
+  describe "increaseBy" $ do
+    it "increases by a correct amount" $
+      increaseBy (dummyDevice 1000 900 1000) (percent' 10) `shouldBe` dummyDevice 1000 1000 1000
 
-      it "does not increase more than the maximum" $
-        increaseBy device' (percent' 10) `shouldBe` device'
+    it "does not increase more than the maximum" $
+      increaseBy device' (percent' 10) `shouldBe` device'
 
-  describe "currentPercentage" $
-    parallel $ do
-      it "returns 100% when the maximum is equal to the current" $
-        currentPercentage device' `shouldBe` 100
+  describe "currentPercentage" $ do
+    it "returns 100% when the maximum is equal to the current" $
+      currentPercentage device' `shouldBe` 100
 
-      it "returns 0% when the current is zero" $ do
-        currentPercentage (dummyDevice 0 0 1000) `shouldBe` 0
+    it "returns 0% when the current is zero" $ do
+      currentPercentage (dummyDevice 0 0 1000) `shouldBe` 0
+
+  describe "percent" $ do
+    it "returns `Nothing` if the value is less than 0" $ do
+      percent (-2) `shouldBe` Nothing
+
+    it "returns `Nothing` if the value is more than 100" $ do
+      percent 104 `shouldBe` Nothing
+
+    it "returns a `Percent` if the value is between 0 and 100" $ do
+      percent 42 `shouldSatisfy` isJust
 
 device' :: BrightnessDevice
 device' = dummyDevice 1000 1000 1000
