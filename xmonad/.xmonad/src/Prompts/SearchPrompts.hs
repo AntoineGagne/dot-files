d1b32334562d4ec25d65a9e9ff13b275baad9ab6
Antoine Gagné <gagnantoine@gmail.com>
2021-02-21 19:51:26 -0500

    Improve search prompt

--- xmonad/.xmonad/src/Prompts/SearchPrompts.hs
+++ xmonad/.xmonad/src/Prompts/SearchPrompts.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NumericUnderscores #-}
+
 module Prompts.SearchPrompts
   ( myPrompt,
   )
@@ -11,15 +13,27 @@ import Themes.Themes
     showColor,
   )
 import qualified XMonad.Prompt as Prompt
+import XMonad.Prompt.FuzzyMatch (fuzzyMatch, fuzzySort)
 
 myPrompt :: Prompt.XPConfig
 myPrompt =
   Prompt.def
-    { Prompt.font = urxvtResourceFontString . font $ myTheme,
+    { Prompt.alwaysHighlight = True,
       Prompt.bgColor = showColor background myTheme,
+      Prompt.bgHLight = showColor background myTheme,
+      Prompt.borderColor = showColor color7 myTheme,
       Prompt.fgColor = showColor foreground myTheme,
       Prompt.fgHLight = showColor color10 myTheme,
-      Prompt.bgHLight = showColor background myTheme,
+      Prompt.font = urxvtResourceFontString promptFont,
+      Prompt.height = 30,
+      Prompt.historyFilter = Prompt.deleteAllDuplicates . Prompt.deleteConsecutive,
+      Prompt.historySize = 30_000,
+      Prompt.maxComplRows = Just 50,
+      Prompt.position = Prompt.CenteredAt {Prompt.xpCenterY = 0.5, Prompt.xpWidth = 1},
       Prompt.promptBorderWidth = 1,
-      Prompt.borderColor = showColor color7 myTheme
+      Prompt.promptKeymap = Prompt.vimLikeXPKeymap,
+      Prompt.searchPredicate = fuzzyMatch,
+      Prompt.sorter = fuzzySort
     }
+  where
+    promptFont = font myTheme
