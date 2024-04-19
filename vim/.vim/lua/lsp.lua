4ce256247b8feb7123ce7d6aca1a78aa188d4b38
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-04-19 14:30:30 -0400

    Refactor LSP configuration

--- vim/.vim/lua/lsp.lua
+++ vim/.vim/lua/lsp.lua
@@ -52,22 +52,27 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+  local keymappings = {
+    ['gD'] = '<Cmd>lua vim.lsp.buf.declaration()<CR>',
+    ['gd'] = '<Cmd>lua vim.lsp.buf.definition()<CR>',
+    ['K'] = '<Cmd>lua vim.lsp.buf.hover()<CR>',
+    ['gi'] = '<cmd>lua vim.lsp.buf.implementation()<CR>',
+    ['<C-k>'] = '<cmd>lua vim.lsp.buf.signature_help()<CR>',
+    ['<space>wa'] = '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>',
+    ['<space>wr'] = '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>',
+    ['<space>wl'] = '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
+    ['<space>D'] = '<cmd>lua vim.lsp.buf.type_definition()<CR>',
+    ['<space>rn'] = '<cmd>lua vim.lsp.buf.rename()<CR>',
+    ['<space>ca'] = '<cmd>lua vim.lsp.buf.code_action()<CR>',
+    ['gr'] = '<cmd>lua vim.lsp.buf.references()<CR>',
+    ['<space>e'] = '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
+    ['[d'] = '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
+    [']d'] = '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
+    ['<space>q'] = '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>'
+  }
+  for keys, mapping in pairs(keymappings) do
+      buf_set_keymap('n', keys, mapping, opts)
+  end
 
   if client.resolved_capabilities then
       -- Set some keybinds conditional on server capabilities
