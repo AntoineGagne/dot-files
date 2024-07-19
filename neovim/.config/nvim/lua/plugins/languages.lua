0558cee2507bca6d9b014891792d61e710a78ca4
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-07-19 16:09:26 -0400

    Add more plugins

---
+++ neovim/.config/nvim/lua/plugins/languages.lua
@@ -0,0 +1,16 @@
+return {
+  { 'HerringtonDarkholme/yats.vim' },
+  { 'vim-erlang/vim-erlang-runtime' },
+  { 'elixir-editors/vim-elixir' },
+  { 'AntoineGagne/perfect-language-vim' },
+  { 'othree/html5.vim' },
+  { 'pangloss/vim-javascript' },
+  { 'mxw/vim-jsx' },
+  { 'andys8/vim-elm-syntax' },
+  { 'cespare/vim-toml' },
+  { 'vito-c/jq.vim' },
+  { 'raichoo/purescript-vim' },
+  { 'vmchale/dhall-vim'},
+  { 'kergoth/vim-bitbake' },
+  { 'hail2u/vim-css3-syntax' }
+}
---
+++ neovim/.config/nvim/lua/plugins/miscellaneous.lua
@@ -0,0 +1,7 @@
+return {
+  { 'tpope/vim-surround' },
+  { 'tpope/vim-abolish' },
+  { 'mbbill/undotree' },
+  { 'tpope/vim-repeat' },
+  { 'editorconfig/editorconfig-vim' },
+}
