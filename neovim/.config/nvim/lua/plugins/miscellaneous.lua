b6cec9a004920898576ba4d4cd4de16b81ef4b4a
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-07-23 15:25:23 -0400

    Update parenthesis style

--- neovim/.config/nvim/lua/plugins/miscellaneous.lua
+++ neovim/.config/nvim/lua/plugins/miscellaneous.lua
@@ -1,7 +1,41 @@
 return {
   { 'tpope/vim-surround' },
   { 'tpope/vim-abolish' },
-  { 'mbbill/undotree' },
+  {
+    'debugloop/telescope-undo.nvim',
+    dependencies = {
+      {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+      },
+    },
+    keys = {
+      -- lazy style key map
+      {
+        '<leader>u',
+        '<cmd>Telescope undo<cr>',
+        desc = 'Undo History',
+      },
+    },
+    opts = {
+      extensions = {
+        undo = {
+          side_by_side = true,
+          layout_strategy = 'vertical',
+          layout_config = {
+            preview_height = 0.8,
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
+      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
+      -- defaults, as well as each extension).
+      require('telescope').setup(opts)
+      require('telescope').load_extension 'undo'
+    end,
+  },
   { 'tpope/vim-repeat' },
   { 'editorconfig/editorconfig-vim' },
 }
