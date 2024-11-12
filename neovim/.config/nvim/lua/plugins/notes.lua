3581a4d512d18e9581d600ce83d5ea15f7e0fc8e
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2024-11-12 14:15:47 -0500

    Add more configuration for neorg

--- neovim/.config/nvim/lua/plugins/notes.lua
+++ neovim/.config/nvim/lua/plugins/notes.lua
@@ -16,7 +16,25 @@ return {
     'nvim-neorg/neorg',
     lazy = false,
     version = '*',
-    config = true,
+    config = function()
+      require('neorg').setup({
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/Documents/notes',
+              },
+              default_workspace = 'notes',
+            },
+          },
+        },
+      })
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
   },
   -- {
   --   'MeanderingProgrammer/markdown.nvim',
