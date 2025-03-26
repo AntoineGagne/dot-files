d366e6d21d3ccf2a8953612e3c48ef386c42412b
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-03-26 14:12:58 -0400

    Disable `friendly-snippets`

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -47,7 +47,7 @@ return {
   {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets' },
+    -- dependencies = { 'rafamadriz/friendly-snippets' },
     version = '1.*',
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
