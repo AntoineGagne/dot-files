1ec93f6cf0a5f06d3475d6673cabfce6dddaec58
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-04-17 13:02:06 -0400

    Remove markdown plugins

--- neovim/.config/nvim/lua/plugins/notes.lua
+++ neovim/.config/nvim/lua/plugins/notes.lua
@@ -1,17 +1,4 @@
 return {
-  -- {
-  --   'OXY2DEV/markview.nvim',
-  --   lazy = false, -- Recommended
-  --   -- ft = "markdown" -- If you decide to lazy-load anyway
-
-  --   dependencies = {
-  --     -- You will not need this if you installed the
-  --     -- parsers manually
-  --     -- Or if the parsers are in your $RUNTIMEPATH
-  --     'nvim-treesitter/nvim-treesitter',
-  --     'nvim-tree/nvim-web-devicons',
-  --   },
-  -- },
   {
     'nvim-neorg/neorg',
     lazy = false,
@@ -35,21 +22,5 @@ return {
       vim.wo.foldlevel = 99
       vim.wo.conceallevel = 2
     end,
-  },
-  {
-    'MeanderingProgrammer/markdown.nvim',
-    main = 'render-markdown',
-    opts = {},
-    -- Only needed if you have another plugin named markdown.nvim
-    -- name = 'render-markdown',
-    -- if you use the mini.nvim suite
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'echasnovski/mini.nvim',
-    },
-    ---- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-    ---- if you prefer nvim-web-devicons
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   },
 }
