6d798f429f42598ffab1fa6b6f0953ac6a8a8416
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-04-18 11:51:01 -0400

    Replace `vim.ui.select` with telescope

--- neovim/.config/nvim/lua/plugins/navigation.lua
+++ neovim/.config/nvim/lua/plugins/navigation.lua
@@ -36,6 +36,12 @@ return {
     end,
   },
   {
+    'nvim-telescope/telescope-ui-select.nvim',
+    config = function()
+      require('telescope').load_extension('ui-select')
+    end,
+  },
+  {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
