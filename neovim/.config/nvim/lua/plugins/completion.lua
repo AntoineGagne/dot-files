5f70136bb066442f123bdad10b12a9a6141696b5
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-05-03 16:14:45 -0400

    Add ghost text for completion

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -69,8 +69,11 @@ return {
         nerd_font_variant = 'mono',
       },
 
-      -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = true } },
+      completion = {
+        -- Automatically show the documentation
+        documentation = { auto_show = true },
+        ghost_text = { enabled = true },
+      },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
