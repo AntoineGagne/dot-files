49ea1a4a42f5a12e46302e4c0fab4b1cdf96b4e7
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2025-05-03 16:14:17 -0400

    Toggle completion menu with `<C-e>`

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -58,7 +58,10 @@ return {
       -- C-k: Toggle signature help (if signature.enabled = true)
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
+      keymap = {
+        preset = 'default',
+        ['<C-e>'] = { 'hide', 'show' },
+      },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
