ecf9079528236c8cf6bce3d570663a07a6f7038b
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2026-01-05 18:45:22 -0500

    Fix rename

--- neovim/.config/nvim/lua/config/autocommands.lua
+++ neovim/.config/nvim/lua/config/autocommands.lua
@@ -14,8 +14,9 @@ local snacks = vim.api.nvim_create_augroup('snacks', { clear = true })
 vim.api.nvim_create_autocmd('User', {
   pattern = 'OilActionsPost',
   callback = function(event)
-    if event.data.actions.type == 'move' then
-      Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+    local actions = event.data.actions[1]
+    if actions[1].type == 'move' then
+      Snacks.rename.on_rename_file(actions.src_url, actions.dest_url)
     end
   end,
   group = snacks,
