a0e0aada6a012d9fb0902b9d7f04f4acafe0f9c1
Antoine Gagné <AntoineGagne@users.noreply.github.com>
2026-09-11 11:06:28 -0400

    feat(clangd): add more flags to clangd

--- neovim/.config/nvim/after/lsp/clangd.lua
+++ neovim/.config/nvim/after/lsp/clangd.lua
@@ -1,1 +1,16 @@
-return {}
+local processors = tonumber(vim.fn.system({ 'nproc' }))
+local processors_flag = ''
+if processors ~= 0 then
+  processors_flag = '--j=' .. (processors - 1)
+end
+
+return {
+  cmd = {
+    'clangd-20',
+    '--clang-tidy',
+    processors_flag,
+    '--header-insertion=iwyu',
+    '--background-index',
+    '--enable-config',
+  },
+}
